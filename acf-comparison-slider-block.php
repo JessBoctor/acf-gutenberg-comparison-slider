0bdca6c5f86aa1c92d805d02ce267ec20bb215b9
Jess Boctor <contactme@jessboctor.com>
2018-11-20 19:57:23 -0800

    Update acf-comparison-slider-block.php

--- acf-comparison-slider-block.php
+++ acf-comparison-slider-block.php
@@ -7,30 +7,20 @@
  
  //Get the post_id
  global $post;
- write_log( $post->ID );
  
  //Get the image size preference
  $image_size = get_field( 'comp_image_size' );
- write_log( 'Image Size: ' . $image_size );
- 
- //Get the image ids
+
+//Get the image ids
  //>>Left Image
  $left_img_id = get_field( 'comp_left_image' );
  $left_img_src = wp_get_attachment_url( $left_img_id ); 
  $left_img_label = get_field('comp_left_image_label');
- 
- write_log( 'Image ID: ' . $left_img_id );
- write_log( 'Image SRC: ');
- write_log(  $left_img_src);
- 
- //>>Right Image
+
+//>>Right Image
  $right_img_id = get_field( 'comp_right_image' );
  $right_img_src = wp_get_attachment_url( $right_img_id );
  $right_img_label = get_field('comp_right_image_label');
- 
- write_log( 'Image ID: ' . $right_img_id );
- write_log( 'Image SRC: ');
- write_log( $right_img_src );
  
  //Get the handle color
  $handle_color = get_field('comp_drag_icon_color');
